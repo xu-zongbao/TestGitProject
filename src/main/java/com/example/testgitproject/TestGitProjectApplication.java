3c9d7b15b6c8aa8665b7100c3f361e0555873db8
xzbxyz <xzbxyz@163.com>
2023-01-09 15:49:05 +0800

    第一次提交

---
+++ src/main/java/com/example/testgitproject/TestGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.testgitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testgitproject/TestGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testgitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
